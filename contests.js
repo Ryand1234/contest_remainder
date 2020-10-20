4c47f898e2e53012b1ff656ad3b8582becec2990
Riyan Dhiman <riyandhiman14@gmail.com>
2020-10-20 16:27:22 +0530

    Update email time and remove unnecessary import.

--- contests.js
+++ contests.js
@@ -1,8 +1,5 @@
 const transporter = require('./transport');
-var sys = require('sys')
 var axios = require('axios')
-var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { console.log(stdout) }
 
 const checkForContest = async () =>{
 	await axios.get('https://contest-remainder-service.herokuapp.com');
@@ -10,15 +7,13 @@ const checkForContest = async () =>{
 	var hour = d.getHours();
 	var minute = d.getMinutes();
 	var second = d.getSeconds();
-	var send = true;
 	console.log("H: ",hour," M: ",minute," S: ",second);
-	if((hour == 8)&&(minute >= 19)&&(minute < 20)&&(second > 0)&&(second < 30)&&(send != true)){ //3600000
+	if((hour == 11)&&(minute >= 4)&&(minute < 5)){ //3600000
 		await transporter();
-		send = true;
-		checkForContest();
+		setTimeout(()=>{checkForContest()}, 900000);
+		return;
 	} else {
 		console.log("NO Data");
-		send = false;
 		setTimeout(()=>{checkForContest()}, 900000);
 		return;	
 	}
