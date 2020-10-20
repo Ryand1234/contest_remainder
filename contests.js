11032f395678ef28d525a983284ff4cf24812138
Riyan Dhiman <Riyandhiman14@gmail.com>
2020-10-20 13:39:34 +0530

    Change time #7

--- contests.js
+++ contests.js
@@ -11,13 +11,13 @@ const checkForContest = async () =>{
 	var minute = d.getMinutes();
 	var second = d.getSeconds();
 	console.log("H: ",hour," M: ",minute," S: ",second);
-	if((hour == 8)&&(minute >= 7)&&(minute < 8)&&(second > 0)&&(second < 10)){ //3600000
+	if((hour == 8)&&(minute >= 10)&&(minute < 11)&&(second > 0)&&(second < 10)){ //3600000
 		console.log("MAIL")
 		await transporter();
 		checkForContest();	
 	} else {
 		console.log("NO Data");
-		setTimeout(()=>{checkForContest()}, 60000);
+		setTimeout(()=>{checkForContest()}, 6000);
 		return;	
 	}
 }
