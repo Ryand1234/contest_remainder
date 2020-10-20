9ec00a898c3407744903ad3a0b66e4e252998871
Riyan Dhiman <Riyandhiman14@gmail.com>
2020-10-20 13:46:19 +0530

    Change time #11

--- contests.js
+++ contests.js
@@ -11,7 +11,7 @@ const checkForContest = async () =>{
 	var minute = d.getMinutes();
 	var second = d.getSeconds();
 	console.log("H: ",hour," M: ",minute," S: ",second);
-	if((hour == 8)&&(minute >= 14)&&(minute < 15)&&(second > 0)&&(second < 30)){ //3600000
+	if((hour == 8)&&(minute >= 17)&&(minute < 18)&&(second > 0)&&(second < 10)){ //3600000
 		console.log("MAIL")
 		await transporter();
 		checkForContest();	
--- transport.js
+++ transport.js
@@ -18,6 +18,9 @@ function time (second) {
 }
 const tranporter = async()=>{
 	const driver = nodemailer.createTransport({
+			host: 'smtp.gmail.com',
+		    port: 465,
+		    secure: true,
 			auth: {
 				user: process.env.EMAIL,
 				pass: process.env.PASSWORD
